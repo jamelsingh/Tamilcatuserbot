5941b4ed83843b83ecf4ceed07b946d14a90ac79
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-07-21 21:24:26 +0530

    Add files via upload

--- userbot/plugins/custom.py
+++ userbot/plugins/custom.py
@@ -7,6 +7,7 @@ from userbot.core.logger import logging
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from . import BOTLOG_CHATID
 
 plugin_category = "utils"
 LOGS = logging.getLogger(__name__)
@@ -91,6 +92,13 @@ async def bad(event):  # sourcery no-metrics
                 if (("PIC" in vname) or ("pic" in vname)) and not url(i):
                     return await edit_delete(event, "**Give me a correct link...**")
             addgvar(vname, vinfo)
+            if BOTLOG_CHATID:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    f"#SET_DATAVAR\
+                    \n**{vname}** is updated newly in database as below",
+                )
+                await event.client.send_message(BOTLOG_CHATID, vinfo, silent=True)
             await edit_delete(
                 event, f"📑 Value of **{vname}** is changed to :- `{vinfo}`", time=20
             )
@@ -101,6 +109,12 @@ async def bad(event):  # sourcery no-metrics
             )
         elif cmd == "del":
             delgvar(vname)
+            if BOTLOG_CHATID:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    f"#DEL_DATAVAR\
+                    \n**{vname}** is deleted from database",
+                )
             await edit_delete(
                 event,
                 f"📑 Value of **{vname}** is now deleted & set to default.",
@@ -164,8 +178,16 @@ async def custom_catuserbot(event):
         urls = extractor.find_urls(reply.text)
         if not urls:
             return await edit_delete(event, "`the given link is not supported`", 5)
-        addgvar("pmpermit_pic", urls)
+        text = " ".join(urls)
+        addgvar("pmpermit_pic", text)
     await edit_or_reply(event, f"__Your custom {input_str} has been updated__")
+    if BOTLOG_CHATID:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"#SET_DATAVAR\
+                    \n**{input_str}** is updated newly in database as below",
+        )
+        await event.client.send_message(BOTLOG_CHATID, text, silent=True)
 
 
 @catub.cat_cmd(
@@ -209,3 +231,9 @@ async def custom_catuserbot(event):
     await edit_or_reply(
         event, f"__successfully deleted your customization of {input_str}.__"
     )
+    if BOTLOG_CHATID:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"#DEL_DATAVAR\
+                    \n**{input_str}** is deleted from database",
+        )
