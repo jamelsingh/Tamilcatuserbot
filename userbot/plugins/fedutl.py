c6992bba949a0bacaf532a1edb7a29f790726d8a
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-08-29 17:15:39 +0530

    Update fedutl.py

--- userbot/plugins/fedutl.py
+++ userbot/plugins/fedutl.py
@@ -197,45 +197,6 @@ async def get_users(show):
     )
 
 
-@catub.cat_cmd(pattern="blist$", command=("blist", plugin_category))
-async def get_users(show):
-    await show.delete()
-    if not show.text[0].isalpha() and show.text[0] not in ("/"):
-        if not show.is_group:
-            await show.edit("Are you sure this is a group?")
-            return
-        info = await show.client.get_entity(show.chat_id)
-        title = info.title if info.title else "this chat"
-        mentions = "id,reason"
-        try:
-            if not show.pattern_match.group(2):
-                async for user in show.client.iter_participants(show.chat_id):
-                    if not user.deleted and user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-                    elif user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-            else:
-                searchq = show.pattern_match.group(2)
-                async for user in show.client.iter_participants(
-                    show.chat_id, search=f"{searchq}"
-                ):
-                    if not user.deleted and user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-                    elif user.id != bot.uid:
-                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
-        except ChatAdminRequiredError as err:
-            mentions += " " + str(err) + "\n"
-        file = open("userslist.csv", "w+")
-        file.write(mentions)
-        file.close()
-        await show.client.send_file(
-            BOTLOG_CHATID,
-            "userslist.csv",
-            caption="Group Members in {}".format(title),
-            reply_to=show.id,
-        )
-
-
 @catub.cat_cmd(pattern="bgban$", command=("bgban", plugin_category))
 async def _(event):
     if G_BAN_LOGGER_GROUP is None:
