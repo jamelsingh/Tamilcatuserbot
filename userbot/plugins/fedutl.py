eb95cafc6075c5ff9fe2d40d3790022c7877da96
jamelsingh <kjamelsingh@gmail.com>
2022-01-30 16:00:45 +0530

    Update fedutl.py

--- userbot/plugins/fedutl.py
+++ userbot/plugins/fedutl.py
@@ -159,44 +159,6 @@ async def _(event):
             await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
 
 
-@catub.cat_cmd(pattern="plist(?:\s|$)([\s\S]*)", command=("plist", plugin_category))
-async def get_users(show):
-    await show.delete()
-    if not show.is_group:
-        await show.edit("Are you sure this is a group?")
-        return
-    input_str = show.pattern_match.group(1)
-    info = await show.client.get_entity(show.chat_id)
-    title = info.title if info.title else "this chat"
-    mentions = "id,reason"
-    try:
-        if not input_str:
-            async for user in show.client.iter_participants(show.chat_id):
-                if not user.deleted and user.id != bot.uid:
-                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
-                elif user.id != bot.uid:
-                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
-        else:
-            async for user in show.client.iter_participants(
-                show.chat_id, search=f"{input_str}"
-            ):
-                if not user.deleted and user.id != bot.uid:
-                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
-                elif user.id != bot.uid:
-                    mentions += f"\n{user.id}, Pornography || {title} || #Massban🔞🛑"
-    except ChatAdminRequiredError as err:
-        mentions += " " + str(err) + "\n"
-    file = open("userslist.csv", "w+")
-    file.write(mentions)
-    file.close()
-    await show.client.send_file(
-        BOTLOG_CHATID,
-        "userslist.csv",
-        caption="Group Members in {}".format(title),
-        reply_to=show.id,
-    )
-
-
 @catub.cat_cmd(pattern="bgban$", command=("bgban", plugin_category))
 async def _(event):
     if G_BAN_LOGGER_GROUP is None:
