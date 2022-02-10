689b7e257fb37bcbffd7dafce95f318653fbab3f
jamelsingh <kjamelsingh@gmail.com>
2022-02-11 00:18:19 +0530

    Update stickerfun.py

--- userbot/plugins/stickerfun.py
+++ userbot/plugins/stickerfun.py
@@ -248,3 +248,29 @@ async def glax(event):
     await hide_inlinebot(
         event.client, bot_name, text, event.chat_id, reply_to_id, c_lick=c_lick
     )
+
+@catub.cat_cmd(
+    pattern="agst ?(.*)",
+    command=("agst", plugin_category),
+    info={
+        "header": " To make animated google sticker",
+        "usage": [
+            "{tr}agst <your text>",
+        ],
+    },
+)
+async def app(deep):
+    
+    if deep.fwd_from:
+        return
+    bot = "@GooglaxBot "
+    text = deep.pattern_match.group(1)
+    reply_to_id = await reply_id(deep)
+    if not text:
+        return await edit_delete(deep, "`Give me some text rip life, Lmao`")
+    else:
+    	    await deep.delete()
+    	    run = await deep.client.inline_query(bot, text)
+    	    result = await run[0].click("me")
+    	    await result.delete()
+    	    await deep.client.send_message(deep.chat_id, result, reply_to=reply_to_id)
