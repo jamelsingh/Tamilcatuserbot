2bf4af7c08cc82fece1da6fe91183d8d05123ad9
jamelsingh <kjamelsingh@gmail.com>
2022-02-13 12:11:14 +0530

    Update client.py

--- userbot/core/client.py
+++ userbot/core/client.py
@@ -122,6 +122,34 @@ class CatUserBotClient(TelegramClient):
                     LOGS.error("Message was same as previous message")
                 except MessageIdInvalidError:
                     LOGS.error("Message was deleted or cant be found")
+                except BotInlineDisabledError:
+                    await edit_delete(check, "`Turn on Inline mode for our bot`", 10)
+                except ChatSendStickersForbiddenError:
+                    await edit_delete(
+                        check, "`I guess i can't send stickers in this chat`", 10
+                    )
+                except BotResponseTimeoutError:
+                    await edit_delete(
+                        check, "`The bot didnt answer to your query in time`", 10
+                    )
+                except ChatSendMediaForbiddenError:
+                    await edit_delete(check, "`You can't send media in this chat`", 10)
+                except AlreadyInConversationError:
+                    await edit_delete(
+                        check,
+                        "`A conversation is already happening with the given chat. try again after some time.`",
+                        10,
+                    )
+                except ChatSendInlineForbiddenError:
+                    await edit_delete(
+                        check, "`You can't send inline messages in this chat.`", 10
+                    )
+                except FloodWaitError as e:
+                    LOGS.error(
+                        f"A flood wait of {e.seconds} occured. wait for {e.seconds} seconds and try"
+                    )
+                    await check.delete()
+                    await asyncio.sleep(e.seconds + 5)
                 except BaseException as e:
                     LOGS.exception(e)
                     if not disable_errors:
